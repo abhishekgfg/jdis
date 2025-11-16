907178f4e8cdae06d06d8ea3a5c439d9feb21023
aviraj0403 <aviraj0403@gmail.com>
2025-11-16 11:15:17 +0530

    all

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true, 
-    allowedHosts: ['all'], 
+    allowedHosts: ['all', 'www.jdinfotechsolutions.in'],
   }
 
 });
